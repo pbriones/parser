33117c1b9b1132d7bc9e4bce7134a4269dc8e9b2
p.briones <p.briones@tcs.com>
2016-08-09 14:35:33 -0400

    added date parser

--- README.md
+++ README.md
@@ -10,8 +10,17 @@ npm install string-parser
 Number
 ---------
 ```
-var numberParser = require('string-parser').Number;
+var number = require('string-parser').Number;
 var text = 'ten thousand five hundred'
 number.getValue(text) //1500
 number.getNumbers(text) //'10000500'
+```
+
+Date
+---------
+```
+var date = require('string-parser').Number;
+
+date.getDate('It is 12/13/2016') //'12/13/2016'
+date.getDate('It is December 13, 2016') //'12/13/2016'
 ```
--- test/date.spec.js
+++ test/date.spec.js
@@ -2,5 +2,13 @@
 var expect = require('chai').expect;
 var date = require('../lib/date');
 
-var longDate = 'Today is August 4, 2016';
-var shortDate = 'Today is 08/04/2016';
+describe('date', function () {
+  it('.getDate should return dates of format mm/dd/yyyy', function () {
+    expect(date.getDate('It is 08-04/2016')).to.equal('08/04/2016');
+    expect(date.getDate('It is 08-04/2016')).to.equal('08/04/2016');
+    expect(date.getDate('It is August 4, 2016')).to.equal('08/04/2016');
+    expect(date.getDate('There is no date here')).to.equal('');
+    expect(date.getDate('There is no date here but 12345678')).to.equal('');
+    expect(date.getDate('There is no date here but 12345678 12312312 12313')).to.equal('');
+  })
+})
