762b340baa034bd90b1cbef2458d7df10893cc57
Phillipe Briones <822533@NOAM.TCS.com>
2016-08-31 17:23:23 -0400

    updated date parser to parse spelled out pre-2000 yeas

--- test/date.spec.js
+++ test/date.spec.js
@@ -7,6 +7,7 @@ describe('date', function () {
     expect(date.getDate('It is 08-04/2016')).to.equal('08/04/2016');
     expect(date.getDate('It is 08-04/2016')).to.equal('08/04/2016');
     expect(date.getDate('It is August 4, 2016')).to.equal('08/04/2016');
+    expect(date.getDate('It is August four nineteen ninety')).to.equal('08/04/1990');
     expect(date.getDate('There is no date here')).to.equal('');
     expect(date.getDate('There is no date here but 12345678')).to.equal('');
     expect(date.getDate('There is no date here but 12345678 12312312 12313')).to.equal('');
